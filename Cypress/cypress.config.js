865c5d9d722721880ef4aaeced320052188be15c
edkbaal <eduard.kolgjini@gmail.com>
2026-04-04 02:53:31 +0200

    Fix cypress config

--- Cypress/cypress.config.js
+++ Cypress/cypress.config.js
@@ -3,11 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      config.env.LOGIN_PAGE = process.env.LOGIN_PAGE
+      config.env.SAUCE_USERNAME = process.env.SAUCE_USERNAME
+      config.env.SAUCE_PASSWORD = process.env.SAUCE_PASSWORD
+      return config
     },
-    env: {
-      LOGIN_PAGE: process.env.LOGIN_PAGE,
-      SAUCE_USERNAME: process.env.SAUCE_USERNAME,
-      SAUCE_PASSWORD: process.env.SAUCE_PASSWORD,
-    }
   },
+  allowCypressEnv: true,
 });
